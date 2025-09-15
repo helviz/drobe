a72ec00a478d55aae7867c8229a9bdea8d73fd9a
Tina <tinaamarion@gmail.com>
2025-09-15 17:26:36 +0300

    Shopping cart functionality

--- drobe/urls.py
+++ drobe/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from products.views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),  
+    path('products/', include('products.urls')),
+    path('cart/', include('cart.urls')),
+    path('account/', include('users.urls')),
+    path('', HomePageView.as_view(), name='dashboard'),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
